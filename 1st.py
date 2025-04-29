128989584e6c084dbdc8c04cf744107219780ee6
purplemetaphysics <sarthakmidhotiya@gmail.com>
2025-04-29 14:06:34 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,14 @@
+# print("Hello world")
+# print("()")
+# print("5")
+# print(5)
+# print(hello world) error no semicullan
+# print("  " " ") exception
+# print("hello world", 7)
+# print("12*7") //here airthmatic operation is also done inside the program
+# print("hello world" 7) #error comma required
+# print(54-98*98/23 , "hello world")
+# print(print(print("hii")))
+# print("hello" , "python" , sep="-" ,  end="!")
+# print("hello"  "pyhton"  "world")
+print("hello" , "python" , "world")
