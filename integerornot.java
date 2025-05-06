d7fea41dec591638f53c65fb4f0421db0a97b2c0
purplemetaphysics <sarthakmidhotiya@gmail.com>
2025-05-06 12:58:44 +0530

    Add files via upload

---
+++ integerornot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class integerornot{
+    public static void main(String[] args){
+        Scanner.sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int inte = sc.hasnextInt();
+
+        if inte==nextInt{
+            
+        }
+    }
+}
